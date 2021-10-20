c225c469e14ab2f0054261c815ecfd8a58e55bb3
HiT-ST05 <khvnpy111@gmail.com>
2021-10-20 12:56:13 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "20/10_11a8",
+    introTitle: 'Xin Chào Các Bông Hồng Của 11a8',
+    introDesc: `Hôm Nay Là 20 tháng 10 
+    tui thay mặt toàn thể các Boy friends Lớp mình
+    chúc các Girl friends có thật nhiều may mắn
+    nhiều sức khoẻ, vui vẻ, hạnh phúc bên gia đình và những người yêu thương❤️`,
+    btnIntro: '❤️Đừng có ấn vào đâyy❤️',
+    title: 'Thằng làm Web có deptrai hongg nè😁',
+    desc: 'lỡ ấn rồi thì mọi người cứ trả lời thật lòng đii nè hihi😊',
+    btnYes: 'DepTraiXuatSac',
+    btnNo: 'Xấu Quỷ Tha Ma Bắt',
+    question: 'u là trời, ngại ghê vậy đó😏',
+    btnReply: 'Gửi😘',
+    reply: 'Hưng vừa đẹp trai vừa tài giỏi, Hưng 10 điểm Hưng số 1.   ',
+    mess: 'Hii xincamon',
+    messDesc: 'Nói Chuyện Một Chút Hongg',
+    btnAccept: '❤️..OK..😍',
+    messLink: 'https://m.me/hit2021.vn' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
